e95176015b6288cc383f3af5cc47dd697d84a317
rinkeshpurohit <rinkeshpurohit@gmail.com>
2015-10-05 01:53:50 +0530

    preload data,remove buttons

--- _site/infinite scroll/js/j.js
+++ _site/infinite scroll/js/j.js
@@ -8,6 +8,9 @@
 		this.$btn = this.container.find(".weatherBtn"),
 		this.template = Handlebars.compile(t),
 		this.url = "http://api.openweathermap.org/data/2.5/weather?q="+this.city+"&APPID=9b905b0fea94db58ab905e386f382c26";
+		for (var i = 0; i <= 6; i++) {
+			this.getJSON(this.appendTemplate);
+		};
 		this.$main.on("scroll",function(e){
 			var st = $(this).scrollTop();
 		    if(self.$main.height()+st >= self.$ul.height()-50){
@@ -16,8 +19,6 @@
 		});
 		this.$btn.on("click",function(e){
 			self.getJSON(self.appendTemplate);
-			self.$main.removeClass('hidden');
-
 		});
 	}
 	Scroll.prototype.getJSON = function (callback){
