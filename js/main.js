f1426e12e23422d2b0c0d5825d1dabc399a79e06
rinkeshpurohit <rinkeshpurohit@gmail.com>
2015-10-06 01:36:59 +0530

    add templates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
-$(function(){
+/*$(function(){
 		$('.container').hover(function() {
 			$(this).find('.bottom').slideDown('slow');
 		}, function() {
 			$(this).find('.bottom').slideUp('slow');
 		});
-	})
+	})*/
+
+$(function(){
+	var timeout;
+	$('.container').hover(
+    function() {
+    	var self = this;
+       	timeout = setTimeout(function(){
+       		if($(self).is(":hover")){
+            	$(self).find('.bottom').slideDown('fast');
+            }
+        },500);
+    },
+    function(){
+        clearTimeout(timeout);
+       $(this).find('.bottom').slideUp('fast');
+    }
+);
+
+})
